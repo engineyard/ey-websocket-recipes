86ef6147b10426826a59f2cde82322bae581361c
Dennis Walters <dwalters@engineyard.com>
2018-10-18 19:03:51 -0400

    Pulled in ey-custom to finish everything up

---
+++ cookbooks/ey-custom/metadata.rb
@@ -0,0 +1,11 @@
+name "ey-custom"
+version '0.0.1'
+
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+
+depends "custom-websocket"
+depends "custom-redis"
+
+issues_url 'https://github.com/engineyard/ey-websocket-recipes/issues'
+source_url 'https://github.com/engineyard/ey-websocket-recipes'
